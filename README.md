6830b881d472d69f1b6e04d0248faa1a086a999f
Clayton <c_w_serious-github@yahoo.com.au>
2018-10-06 23:45:19 +1000

    UPDATE readme, found ways to validate email addresses

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 Tutorials used:
+
 * https://www.sitepoint.com/working-with-files-in-php/
+
+  To find out how to work with files.
+
 * http://tizag.com/phpT/php-string-strtoupper-strtolower.php
+
+  To find out how to change strings to lower case and capitalize.
+
+Other sites and help used:
+
+* http://fr.php.net/manual/en/function.filter-var.php
+* https://stackoverflow.com/questions/13447539/php-preg-match-with-email-validation-issue
+
+  To find out how to validate emails properly, especially the 'phil@open.edu.au' 'not valid' problem
--- user-upload.php
+++ user-upload.php
@@ -1,22 +1,33 @@
 <?php
+  function validate_email($email) {
+    $pattern = "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,})$/i";
+    return preg_match($pattern, $email);
+  }
 
   $file = "users.csv";
   
-  if(file_exists($file)){ // Check the existence of file
-    $f = fopen($file, "r") or die("ERROR: Cannot open the file."); // Open file for reading or give error message if you can't open file
+  if (file_exists($file)) { // Check the existence of file
+    $f = fopen($file, "r") or die ("ERROR: Cannot open the file."); // Open file for reading or give error message if you can't open file
     $headers_read = 0;
     $field_no = 1; 
 
     while ($record = fgetcsv($f)) { // Loop through csv file 1 record at a time
-      foreach($record as $field) {     // Loop through fields in current record
-        if($headers_read == 3 ) {         // If you have read the headers name, surname & email
-          $field = strtolower($field);       // then make fieldstring lowercase
-          
-          if($field_no < 3) {                // If field is firstname or lastname
-            $field = ucwords($field);          // then capitalize it
-            $field_no = $field_no + 1;         // and look at next field
+      foreach ($record as $field)   {  // Loop through fields in current record
+        if ($headers_read === 3 )      {   // If you have read the headers name, surname & email
+          //$field = strtolower($field);       // then make fieldstring lowercase
+          $field = strtolower( trim($field) );
+
+          if ($field_no < 3) {               // If field is firstname or lastname
+            $field = ucwords($field);           // then capitalize it
+            $field_no = $field_no + 1;          // and look at next field
           }
           else {                             // Else must be at email
+            //if (!filter_var($field, FILTER_VALIDATE_EMAIL)) {
+            //if (!filter_var(trim($field), FILTER_VALIDATE_EMAIL)) { // Check for invalid email address
+            if ( !validate_email($field) ) {
+            //if ( !validate_email( trim($field) ) ) {
+                echo "INVALID EMAIL ADDRESS GIVEN!\n";
+            }
             $field_no = 1;                     // so look at first field from next record   
           }
           echo $field . "\n";                // Print each field
