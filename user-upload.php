d4382a73eb56acaa25eba8942ae7d9f492faf131
Clayton <c_w_serious-github@yahoo.com.au>
2018-10-06 21:33:50 +1000

    Can capitalise name & surname, can set email to lowercase

--- user-upload.php
+++ user-upload.php
@@ -5,11 +5,21 @@
   if(file_exists($file)){ // Check the existence of file
     $f = fopen($file, "r") or die("ERROR: Cannot open the file."); // Open file for reading or give error message if you can't open file
     $headers_read = 0;
+    $field_no = 1; 
 
     while ($record = fgetcsv($f)) { // Loop through csv file 1 record at a time
       foreach($record as $field) {     // Loop through fields in current record
         if($headers_read == 3 ) {         // If you have read the headers name, surname & email
-          echo $field . "\n";               // Print each field
+          $field = strtolower($field);       // then make fieldstring lowercase
+          
+          if($field_no < 3) {                // If field is firstname or lastname
+            $field = ucwords($field);          // then capitalize it
+            $field_no = $field_no + 1;         // and look at next field
+          }
+          else {                             // Else must be at email
+            $field_no = 1;                     // so look at first field from next record   
+          }
+          echo $field . "\n";                // Print each field
         }
         else {                            // else read in "Headers" name, surname & email
           $headers_read = $headers_read + 1;
